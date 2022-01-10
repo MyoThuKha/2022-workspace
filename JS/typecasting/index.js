0e908abf5a3de9d9571a10dca79e69dd014cc7a6
MyoThuKha <myothukha@outlook.sg>
2022-01-10 21:28:54 +0630

    ...

---
+++ JS/typecasting/index.js
@@ -0,0 +1,11 @@
+const value =32;
+
+//Explicit typecasting.
+
+const str_value = String(value);
+
+//Implicit typecasting.
+const imp_string = value+"";
+
+console.log(typeof str_value);
+console.log(typeof imp_string);
