1c32192a216134c236a0e1d9146332820ac8a1e2
MyoThuKha <myothukha@outlook.sg>
2022-01-12 11:56:52 +0630

    ...

--- JS/objects/hoisting.js
+++ JS/objects/hoisting.js
@@ -1,4 +1,5 @@
 hoisting();
+cantHoist();
 
 var x = 3;
 var y = 67;
@@ -7,5 +8,9 @@ var z = 'Hello nice to meet you.';
 //You can call function above function. it is called (hoisting).
 
 function hoisting(){
-    console.log("Hello world!");
+    console.log("Hoisted");
 }
+
+//cannot use in some case. (const cannot use before it declare. Error)
+
+const cantHoist = function(){console.log("Can't Hoist");};
