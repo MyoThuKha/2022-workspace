81ea6d5465c6903b5bdfdbf66106b78d32697d80
MyoThuKha <myothukha@outlook.sg>
2022-01-12 11:39:31 +0630

    ...

---
+++ JS/objects/hoisting.js
@@ -0,0 +1,11 @@
+hoisting();
+
+var x = 3;
+var y = 67;
+var z = 'Hello nice to meet you.';
+
+//You can call function above function. it is called (hoisting).
+
+function hoisting(){
+    console.log("Hello world!");
+}
