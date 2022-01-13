698f23a9f5ffdcee4f7165243c32bf14b92ec6fe
MyoThuKha <myothukha@outlook.sg>
2022-01-13 22:48:15 +0630

    ...

--- JS/hcf/implement.js
+++ JS/hcf/implement.js
@@ -8,8 +8,30 @@ function map(arr,func){
     return newArr;
 }
 
-arr = [0,1,2,3];
 
-const addOne = function (num){return (num+1)};
+//filter implementation
+function filter(arr,func){
+    const newArr = [];
+    for (let i=0;i<arr.length;i++){
+        if (func(arr[i])){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+
+
+//reduce implementation
+function reduce(arr,func){
+   return ("Unfinished work!!! Implement here")
+}
+
+
+const addOne = function(num){return (num+1)};
+const grtTwo = function(num){return (num>2)};
+const add = function(x,y){return (x+y)};
+arr = [0,1,2,3,4];
 
 console.log(map(arr,addOne));
+console.log(filter(arr,grtTwo));
+console.log(reduce(arr,add));
