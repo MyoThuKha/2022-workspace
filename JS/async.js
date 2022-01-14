419cb79156862beb21a4fd364bb635f09d16314e
MyoThuKha <myothukha@outlook.sg>
2022-01-14 13:35:23 +0630

    ...

---
+++ JS/async.js
@@ -0,0 +1,10 @@
+function printOne(){console.log("One")};
+
+function printTwo(){console.log("Two")};
+
+function printThree(){console.log("Three")};
+
+
+setTimeout(printOne, 1000);
+setTimeout(printTwo, 0);
+printThree();
