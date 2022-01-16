b6454e722e122e52b80b0020c3742afd70d50ba5
MyoThuKha <myothukha@outlook.sg>
2022-01-16 22:49:00 +0630

    ...

--- pyramid/index.js
+++ pyramid/index.js
@@ -1,8 +1,11 @@
 let star_row ="";
 const num_row = 8;
 for (let i=1;i <= num_row; i++){
-    for (let j=0; j < i; j++){
-        star_row = star_row + "*";
+    for (let j=0; j < (num_row-i); j++){
+        star_row = star_row + " ";
+    }
+    for (let k =0;k < i; k++){
+        star_row += "* ";
     }
     console.log(star_row);
     star_row = "";
