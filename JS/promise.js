9a68870a8e03441053ba0119e2fb82986e78cde8
MyoThuKha <myothukha@outlook.sg>
2022-01-16 19:12:44 +0630

    ...

---
+++ JS/promise.js
@@ -0,0 +1,14 @@
+const url = '';
+
+fetch(url){
+    .then(function(result){
+    })
+    .then(function(json){
+    })
+    .then(function(data){
+    })
+
+    .catch(function(err){
+        //handle error
+    })
+}
