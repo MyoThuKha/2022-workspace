03fbafb56a8c4424154658d7ed82493fa4b53064
MyoThuKha <myothukha@outlook.sg>
2022-01-17 18:27:40 +0630

    ...

---
+++ binary_tree/first.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+root = Node(4)
+node2 = Node(9)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(6)
+node6 = Node(1)
+
+root.left = node2
+root.right = node3
+
+node2.left = node4
+node2.right = node5
+
+node3.right = node6
+
