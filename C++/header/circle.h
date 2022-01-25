30100995abe7f141693b6e0710c1a895f642f0ad
MyoThuKha <myothukha@outlook.sg>
2022-01-25 18:36:52 +0630

    ...

---
+++ C++/header/circle.h
@@ -0,0 +1,16 @@
+#include <cmath>
+        
+double area(double r)
+{
+    return (M_PI * r * r);
+}
+
+double parameter(double r)
+{
+    return (2 * M_PI * r);
+}
+
+double sphereArea(double r)
+{
+    return (4/3 * M_PI * r * r * r);
+}
