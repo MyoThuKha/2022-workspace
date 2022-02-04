e9ee240b3d4e347c9f941542d5ce5d12f8267e5c
MyoThuKha <myothukha@outlook.sg>
2022-02-04 18:41:56 +0630

    ...

---
+++ pyGame/revision.py
@@ -0,0 +1,9 @@
+#Note that this file will change every time the revision goes.
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,400))
+
+while True:
+    pygame.display.update()
