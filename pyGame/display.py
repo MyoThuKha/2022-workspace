f15897ce6410381471913ba94589e47407ad0187
MyoThuKha <myothukha@outlook.sg>
2022-02-05 18:52:01 +0630

    ...

--- pyGame/display.py
+++ pyGame/display.py
@@ -5,5 +5,8 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 
 while True:
-    #Here code
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
     pygame.display.update()
+
