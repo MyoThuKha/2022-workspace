880f50cfb9f68e7a7d79b4ad76c03e513174d394
MyoThuKha <myothukha@outlook.sg>
2022-02-10 21:19:59 +0630

    ...

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
+package codecamp;
+//Pascal naming convention
 public class Main {
 	public static void main(String[] args) {
+		//Camel naming convention
 		int myAge = 18;
-        int herAge = myAge;
+		int herAge = myAge;
+
 		System.out.println("My age is "+myAge);
-        System.out.println("Her age is "+herAge);
+		System.out.println("Her age is "+herAge);
 	}
 }
