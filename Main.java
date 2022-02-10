6d3fefa107cafa97cbcb53b1871d9b17b716147e
MyoThuKha <myothukha@outlook.sg>
2022-02-10 21:08:57 +0630

    ...

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package codecamp;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		int age = 18;
+		System.out.println("My age is "+age);
 	}
 }
