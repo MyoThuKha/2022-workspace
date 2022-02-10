c61895cc1a709d66db0ea44c11f28d6213008a9b
MyoThuKha <myothukha@outlook.sg>
2022-02-10 21:14:25 +0630

    ...

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-package codecamp;
-
 public class Main {
 	public static void main(String[] args) {
-		int age = 18;
-		System.out.println("My age is "+age);
+		int myAge = 18;
+        int herAge = myAge;
+		System.out.println("My age is "+myAge);
+        System.out.println("Her age is "+herAge);
 	}
 }
