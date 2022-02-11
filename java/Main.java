7a581c855db108c79d193b240a3e537bd7af5f65
MyoThuKha <myothukha@outlook.sg>
2022-02-11 11:32:56 +0630

    ...

--- java/Main.java
+++ java/Main.java
@@ -1,21 +1,21 @@
-package codecamp;
 //Pascal naming convention
 public class Main {
-	public static void main(String[] args) {
-		//Camel naming convention
-		byte myAge = 23;
-		byte herAge = myAge;
-		
-		boolean isMale = true;
-		
-		int id = 334_383;
-		long count = 3_444_634_343L;
-		
-		float decimal = 4.44F;
-		
-		double deciLong =454.343;
+    public static void main(String[] args) {
+        //Camel naming convention
+        byte myAge = 23;
+        byte herAge = myAge;
 
-		System.out.println("My age is "+myAge);
-		System.out.println("Her age is "+herAge);
-	}
+        boolean isMale = true;
+
+        int id = 334_383;
+        long count = 3_444_634_343L;
+        //F for converting from double to float.
+        float decimal = 4.44F;
+        double deciLong =454.343;
+        //single quote for char
+        char randomChar = 'A';
+        System.out.println("My age is "+myAge);
+        System.out.println("Her age is "+herAge);
+
+    }
 }
