308756c5f5c1d1fbb245c896b49f93ba7e9bcd8f
MyoThuKha <myothukha@outlook.sg>
2022-02-12 18:28:14 +0630

    ...

---
+++ java/Arrj.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Arrj{
+    public static void main(String[] args){
+        //total element of 6.
+        int[] numbers = new int[6];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
