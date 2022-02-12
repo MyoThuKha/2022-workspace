a22f660abe34a8f1070d1408f8be801ccc417213
MyoThuKha <myothukha@outlook.sg>
2022-02-12 19:29:03 +0630

    ...

--- java/StringClass.java
+++ java/StringClass.java
@@ -1,10 +1,9 @@
-package codecamp;
-
 public class StringClass {
     public static void main(String [] args) {
+        //String is the reference type.
+        //String greet = new String("Hello");
         String greet = "Hello";
         System.out.println(greet);
-        
         System.out.println(greet.toUpperCase());
         System.out.println(greet.length());
     }
