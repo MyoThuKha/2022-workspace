947716b39f79ddf5d9433700065b14ab2095603e
MyoThuKha <myothukha@outlook.sg>
2022-02-12 19:27:19 +0630

    ...

---
+++ java/StringClass.java
@@ -0,0 +1,11 @@
+package codecamp;
+
+public class StringClass {
+    public static void main(String [] args) {
+        String greet = "Hello";
+        System.out.println(greet);
+        
+        System.out.println(greet.toUpperCase());
+        System.out.println(greet.length());
+    }
+}
