e64411366fc25cedb118c3a81ef402dcc8b7b06d
MyoThuKha <myothukha@outlook.sg>
2022-02-12 19:30:22 +0630

    ...

--- java/Arrj.java
+++ java/Arrj.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 public class Arrj{
     public static void main(String[] args){
         //total element of 6.
+        //array is reference type.
         int[] numbers = new int[6];
         numbers[0] = 1;
         numbers[1] = 2;
