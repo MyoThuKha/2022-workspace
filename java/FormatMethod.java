24689775dad2571f92bf30811e431cf7f5bfa6e7
MyoThuKha <myothukha@outlook.sg>
2022-02-13 20:25:49 +0630

    ...

--- java/FormatMethod.java
+++ java/FormatMethod.java
@@ -1,7 +1,7 @@
 import java.text.NumberFormat;
 public class FormatMethod {
     public static void main(String[] args) {
-        //can't use new operator cause abstract class.
+        //can't use new operator cause it is abstract class.
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         String price = currency.format(3435299L);
         System.out.println(price);
