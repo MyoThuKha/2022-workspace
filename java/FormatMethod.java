a715dacb02221aaf96ebfb43596c204aa2bb434f
MyoThuKha <myothukha@outlook.sg>
2022-02-13 20:24:54 +0630

    ...

---
+++ java/FormatMethod.java
@@ -0,0 +1,12 @@
+import java.text.NumberFormat;
+public class FormatMethod {
+    public static void main(String[] args) {
+        //can't use new operator cause abstract class.
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String price = currency.format(3435299L);
+        System.out.println(price);
+        
+        String percent = NumberFormat.getPercentInstance().format(0.35);
+        System.out.println(percent);
+    }
+}
