203003cb438fe054217d01af1a2b1983710999be
MyoThuKha <myothukha@outlook.sg>
2022-02-16 21:08:57 +0630

    ...

--- java/Revison.java
+++ java/Revison.java
@@ -1,1 +1,8 @@
-package codecamp;
+import java.util.Scanner;
+
+public class Revision {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine("Enter your name: ");
+    }
+}
