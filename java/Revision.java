0492ea69f9a3b4ed6854b0afa7589dc25cb6d005
MyoThuKha <myothukha@outlook.sg>
2022-02-17 21:45:09 +0630

    ...

--- java/Revision.java
+++ java/Revision.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 public class Revision {
     public static void main(String[] args) {
         String num1 = "33830";
+
+        System.out.println(Integer.parseInt(num1)+1);
         
         int[][] matx = {{2,42,4},{24,83,72}};
 
-        System.out.println(Arrays.toString(matx));
-        System.out.println(Integer.parseInt(num1)+1);
+        System.out.println(Arrays.deepToString(matx));
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Your Name: ");
         String name = scan.nextLine();
-        System.out.println("Hi "+name);
+        System.out.println("Hi "+name.trim());
     }
 }
