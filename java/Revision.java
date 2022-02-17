e407a00ab55f5bfc568104fff15b4311fcf11780
MyoThuKha <myothukha@outlook.sg>
2022-02-17 20:56:01 +0630

    ...

--- java/Revision.java
+++ java/Revision.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Revision {
     public static void main(String[] args) {
+        String num1 = "33830";
+        
+        System.out.println(Integer.parseInt(num1)+1);
         Scanner scan = new Scanner(System.in);
         System.out.print("Your Name: ");
         String name = scan.nextLine();
