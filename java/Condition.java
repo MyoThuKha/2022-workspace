45187dbc946aa55c8a70f8ba7c6d4ab2d786f0ea
MyoThuKha <myothukha@outlook.sg>
2022-02-18 17:57:32 +0630

    ...

--- java/Condition.java
+++ java/Condition.java
@@ -2,7 +2,10 @@ public class Condition {
     public static void main(String[] args) {
         int score = 83;
         
-        //Single statement condition doesn't need {}.
+        //Single statement condition doesn't need {} 
+        // can't declare variable in if. cause outside {} guys can't access it.
+        //so declare variable outside if scope.
+
         if (score > 80) {
             System.out.println("Honor");
             System.out.println("pass");
@@ -11,5 +14,10 @@ public class Condition {
             System.out.println("pass");
         else
             System.out.println("Fail");
+        
+        //Simplify
+        boolean isPerfect = (score == 100);
+        System.out.println(isPerfect);
+        
     }
 }
