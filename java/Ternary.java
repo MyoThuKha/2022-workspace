daa7fc53c9279ac04f1f46548d8c7d5c87f43c36
MyoThuKha <myothukha@outlook.sg>
2022-02-19 10:45:23 +0630

    ...

--- java/Ternary.java
+++ java/Ternary.java
@@ -1,5 +1,7 @@
-public class ternary {
+public class Ternary {
     public static void main(String[] args) {
-        
+        int income = 135_000;
+        String flightClass = income > 100_000? "Business":"Economy";
+        System.out.println(flightClass);
     }
 }
