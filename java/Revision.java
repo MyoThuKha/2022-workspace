cee9047852cdeecfb6e715624dfa1a8d50dc9589
MyoThuKha <myothukha@outlook.sg>
2022-02-28 20:08:31 +0630

    ...

--- java/Revision.java
+++ java/Revision.java
@@ -1,6 +1,9 @@
-import java.util.Scanner;
+import java.util.Arrays;
+
 public class Revision {
     public static void main(String[] args) {
-        
+        int[] numbers = {1,2,2,5,35,4};
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
