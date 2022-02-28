4f3e1e8156f016f7972d52de24dc0c0e850705b8
MyoThuKha <myothukha@outlook.sg>
2022-02-28 20:03:11 +0630

    ...

--- java/Revision.java
+++ java/Revision.java
@@ -2,10 +2,5 @@ import java.util.Scanner;
 public class Revision {
     public static void main(String[] args) {
         
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter income: ");
-        int income = scan.nextInt();
-        String seat = income > 100_000? "First class":"Economy";
-        System.out.println(seat);
     }
 }
