77fd537337f8ea164e77f2bdf33387d7bd43cf98
MyoThuKha <myothukha@outlook.sg>
2022-03-01 18:24:47 +0630

    ...

--- java/Looping.java
+++ java/Looping.java
@@ -3,5 +3,12 @@ public class Looping {
         for (int i=0; i < 5; i++) {
             System.out.println(i);
         }
+
+
+        int j = 0;
+        while (j < 5) {
+            System.out.println("while loop" + j);
+            j++;
+        }
     }
 }
