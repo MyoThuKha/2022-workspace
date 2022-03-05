f668a0faf0615e616d0136958cfaa34f1ff75e1b
MyoThuKha <myothukha@outlook.sg>
2022-03-05 19:19:15 +0630

    ...

---
+++ dart/custom.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[200],
+        title: const Text("App Title"),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextButton(
+            onPressed: () {},
+            child: Container(
+              padding: const EdgeInsets.all(60.0),
+              color: Colors.blue[100],
+              child: const Center(
+                child: Text(
+                  "First box",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Container(
+              padding: const EdgeInsets.all(60.0),
+              color: Colors.pink[100],
+              child: const Center(
+                  child: Text(
+                "second box",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              )),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Container(
+              padding: const EdgeInsets.all(60.0),
+              color: Colors.yellow[300],
+              child: const Center(
+                  child: Text(
+                "third box",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              )),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue[200],
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
