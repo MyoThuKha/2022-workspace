57146c633e21ecd55efbaf0bf8405b68a324fd04
MyoThuKha <myothukha@outlook.sg>
2022-03-20 14:48:19 +0630

    ...

---
+++ dart/world_time/lib5/Pages/choose_location.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/time_template.dart';
+
+class Location extends StatefulWidget {
+  const Location({Key? key}) : super(key: key);
+
+  @override
+  State<Location> createState() => _LocationState();
+}
+
+class _LocationState extends State<Location> {
+  List<TimeTemplate> locations = [
+    TimeTemplate(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    TimeTemplate(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    TimeTemplate(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    TimeTemplate(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    TimeTemplate(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    TimeTemplate(
+        url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    TimeTemplate(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    TimeTemplate(
+        url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Choose Location"),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Card(
+                elevation: 2,
+                child: ListTile(
+                  onTap: () {},
+                  title: Text(locations[index].location),
+                ),
+              ),
+            );
+          },
+        ));
+  }
+}
---
+++ dart/world_time/lib5/Pages/home.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Map data = {};
+  @override
+  Widget build(BuildContext context) {
+    data = ModalRoute.of(context)?.settings.arguments as Map;
+    String bgImage = data['isDay'] ? 'light.jpg' : 'dark.jpg';
+    Color? cardColor = data['isDay'] ? Colors.white70 : Colors.black54;
+    Color? fontColor = data['isDay'] ? Colors.black : Colors.grey[200];
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/$bgImage'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 150.0, 30, 200.0),
+          child: Card(
+            elevation: 10.0,
+            color: cardColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: fontColor,
+                  ),
+                  label: Text(
+                    "Edit Location",
+                    style: TextStyle(color: fontColor),
+                  ),
+                  style: const ButtonStyle(),
+                ),
+                const SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        color: fontColor,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 60.0,
+                    color: fontColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
