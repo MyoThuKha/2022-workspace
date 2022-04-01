f9a970f1f34445e083a905944027a891fbabe521
MyoThuKha <myothukha@outlook.sg>
2022-04-01 18:53:02 +0630

    ...

--- dart/noteapp/lib/Pages/home.dart
+++ dart/noteapp/lib/Pages/home.dart
@@ -30,80 +30,78 @@ class _HomePageState extends State<HomePage> {
     notes = notes.isEmpty
         ? ModalRoute.of(context)?.settings.arguments as List<Note>
         : notes;
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text(
-            "Notes",
-            style: TextStyle(color: Colors.black),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          elevation: 1.0,
-
-          //AppBar actions button
-          actions: <Widget>[
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu_rounded),
-              color: Colors.black,
-            )
-          ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Notes",
+          style: TextStyle(color: Colors.black),
         ),
-        body: Container(
-          margin: const EdgeInsets.all(12),
-          child: MasonryGridView.count(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 12,
-            itemCount: notes.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    Navigator.pushNamed(context, '/edit', arguments: {
-                      'id': notes[index].id,
-                      'title': notes[index].title,
-                      'context': notes[index].context,
-                    });
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 1.0,
+
+        //AppBar actions button
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu_rounded),
+            color: Colors.black,
+          )
+        ],
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(12),
+        child: MasonryGridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 12,
+          itemCount: notes.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  Navigator.pushNamed(context, '/edit', arguments: {
+                    'id': notes[index].id,
+                    'title': notes[index].title,
+                    'context': notes[index].context,
                   });
-                },
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    padding: const EdgeInsets.all(15.0),
-                    color: noteColor,
-                    child: Column(
-                      children: <Widget>[
-                        AutoSizeText(
-                          notes[index].title,
-                          style: const TextStyle(fontSize: 20),
-                          maxLines: 1,
-                        ),
-                        const SizedBox(height: 10),
-                        AutoSizeText(
-                          notes[index].context,
-                          style: const TextStyle(fontSize: 20),
-                          minFontSize: 10,
-                          maxFontSize: 14,
-                          maxLines: 8,
-                        ),
-                      ],
-                    ),
+                });
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  padding: const EdgeInsets.all(15.0),
+                  color: noteColor,
+                  child: Column(
+                    children: <Widget>[
+                      AutoSizeText(
+                        notes[index].title,
+                        style: const TextStyle(fontSize: 20),
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 10),
+                      AutoSizeText(
+                        notes[index].context,
+                        style: const TextStyle(fontSize: 20),
+                        minFontSize: 10,
+                        maxFontSize: 14,
+                        maxLines: 8,
+                      ),
+                    ],
                   ),
                 ),
-              );
-            },
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/new');
+              ),
+            );
           },
-          child: const Icon(Icons.add),
-          backgroundColor: Colors.orange,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/new');
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.orange,
       ),
     );
   }
