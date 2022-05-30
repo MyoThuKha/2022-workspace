f6fa7e8eeb2cdcaaf7033e96673d8a4cdde20e87
MyoThuKha <myothukha@outlook.sg>
2022-05-30 16:22:06 +0630

    ...

--- flutter/music_app/lib/Pages/home.dart
+++ flutter/music_app/lib/Pages/home.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,13 +8,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List music = [
+    'Harry Potter',
+    'Fantastic Beasts1',
+    'Harry Potter2',
+    'Fantastic Beasts2',
+    'Harry Potter3',
+    'Fantastic Beasts3',
+    'Harry Potter4',
+    'Fantastic Beasts4',
+    'Harry Potter5',
+    'Fantastic Beasts5',
+  ];
   @override
   Widget build(BuildContext context) {
+    double appWidth = MediaQuery.of(context).size.width;
+    double appHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Playlist"),
-        centerTitle: true,
-        elevation: 0,
+        title: const Text(
+          "Music",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
+        actions: <Widget>[],
+      ),
+      body: Row(
+        children: <Widget>[
+          Material(
+            elevation: 2,
+            child: Container(
+              width: appWidth / 5,
+              height: appHeight,
+              color: Colors.white,
+              child: Column(),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: music.length,
+              itemBuilder: (context, index) => Container(
+                width: appWidth / 5 * 4,
+                height: appHeight / 12,
+                color: index / 2 == 0 ? Colors.grey : Colors.white,
+                child: Text(music[index]),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: appHeight / 8,
+        ),
       ),
     );
   }
--- flutter/music_app/lib/main.dart
+++ flutter/music_app/lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:music_app/Pages/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => const HomePage(),
-    },
+  runApp(const MaterialApp(
+    title: "Music App",
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
   ));
 }
