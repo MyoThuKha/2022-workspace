428cf8b372501194087e939f0acf5c07867f58eb
MyoThuKha <myothukha@outlook.sg>
2022-05-30 10:54:28 +0630

    First set up

---
+++ flutter/music_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/Pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomePage(),
+    },
+  ));
+}
