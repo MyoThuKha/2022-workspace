5390fd75c35b2c4ac84f12dfb88d8556f0c97bf0
MyoThuKha <myothukha@outlook.sg>
2022-06-20 10:29:22 +0630

    ...

--- flutter/music_app/lib/main.dart
+++ flutter/music_app/lib/main.dart
@@ -1,11 +1,5 @@
-import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:music_app/Pages/home.dart';
 
 void main() {
-  runApp(FluentApp(
-    title: "Music App",
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
+  runApp(const MaterialApp());
 }
