5734b125d88304497d8a3222fabfe024027e97dc
MyoThuKha <myothukha@outlook.sg>
2022-06-20 19:01:14 +0630

    ...

--- flutter/music_app/lib/main.dart
+++ flutter/music_app/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/Pages/home.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(MaterialApp(
+    initialRoute: "/",
+    routes: {
+      "/": (context) => const HomePage(),
+    },
+  ));
 }
