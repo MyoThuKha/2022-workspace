df9fb8426dfc08f0297be77295e1a538d7610eb8
MyoThuKha <myothukha@outlook.sg>
2022-06-21 11:11:12 +0630

    ...

--- flutter/music_app/lib/Pages/home.dart
+++ flutter/music_app/lib/Pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Home",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.grey[50],
+        elevation: 0,
+      ),
+      body: Container(),
+      bottomNavigationBar: Material(
+        elevation: 1,
+        child: FloatingNavbar(
+          backgroundColor: Colors.white,
+          onTap: (int val) {
+            //returns tab id which is user tapped
+          },
+          currentIndex: 0,
+          items: [
+            FloatingNavbarItem(icon: Icons.home, title: 'Home'),
+            FloatingNavbarItem(icon: Icons.explore, title: 'Explore'),
+            FloatingNavbarItem(icon: Icons.chat_bubble_outline, title: 'Chats'),
+            FloatingNavbarItem(icon: Icons.settings, title: 'Settings'),
+          ],
+        ),
+      ),
+    );
   }
 }
