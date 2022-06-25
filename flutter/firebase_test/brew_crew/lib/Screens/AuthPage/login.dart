b0f3bddd9069c8b27236a87a0cb86035f9a78e9e
MyoThuKha <myothukha@outlook.sg>
2022-06-25 17:32:07 +0630

    added anys login

--- flutter/firebase_test/brew_crew/lib/Screens/AuthPage/login.dart
+++ flutter/firebase_test/brew_crew/lib/Screens/AuthPage/login.dart
@@ -1,3 +1,5 @@
+import 'package:brew_crew/Services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 
@@ -10,6 +12,13 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool _isElevated = true;
+
+  final AuthService _auth = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +44,20 @@ class _LoginPageState extends State<LoginPage> {
         ),
         child: Center(
           child: GestureDetector(
-            onTap: () {
+            onTap: () async {
               setState(() {
                 _isElevated = !_isElevated;
               });
+              dynamic result = await _auth.signAsAnys();
+              if (result == null) {
+                print("Failed");
+              } else {
+                print("Success");
+                print(result);
+              }
             },
             child: AnimatedContainer(
-                duration: const Duration(milliseconds: 150),
+                duration: const Duration(milliseconds: 200),
                 width: 100,
                 height: 100,
                 decoration: BoxDecoration(
@@ -61,12 +77,25 @@ class _LoginPageState extends State<LoginPage> {
                               blurRadius: 15,
                               spreadRadius: 1)
                         ]
-                      : null,
+                      : [
+                          const BoxShadow(
+                            color: Colors.green,
+                            offset: Offset(2, 2),
+                            blurRadius: 15,
+                            spreadRadius: 1,
+                          ),
+                          const BoxShadow(
+                              color: Colors.white,
+                              offset: Offset(-2, -2),
+                              blurRadius: 15,
+                              spreadRadius: 1)
+                        ],
                 ),
                 child: Icon(
                   _isElevated
                       ? CupertinoIcons.person_solid
                       : CupertinoIcons.checkmark_alt,
+                  color: _isElevated ? Colors.black : Colors.green,
                   size: 30,
                 )),
           ),
