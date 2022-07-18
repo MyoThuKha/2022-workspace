c2f0be011a597f6103fa2b474f957caa70dfef33
MyoThuKha <myothukha@outlook.sg>
2022-07-18 17:27:19 +0630

    created menu db

--- flutter/firebase_test/brew_crew/lib/Screens/Pages/coffee.dart
+++ flutter/firebase_test/brew_crew/lib/Screens/Pages/coffee.dart
@@ -1,4 +1,7 @@
+import 'package:brew_crew/Models/menu_model.dart';
+import 'package:brew_crew/Services/brewdb.dart';
 import 'package:brew_crew/Templates/colors.dart';
+import 'package:brew_crew/Templates/loading.dart';
 import 'package:flutter/material.dart';
 
 class CoffeeList extends StatefulWidget {
@@ -11,116 +14,78 @@ class CoffeeList extends StatefulWidget {
 class _CoffeeListState extends State<CoffeeList> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //Coffee part
-        Container(
-          height: 150,
-          margin: const EdgeInsets.symmetric(horizontal: 10),
-          // decoration: BoxDecoration(
-          //   color: customGreyColor,
-          //   borderRadius: BorderRadius.circular(15),
-          //   boxShadow: customShadow,
-          // ),
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  padding: const EdgeInsets.all(15),
-                  height: 100,
-                  margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                  decoration: BoxDecoration(
-                    color: coffeeColor,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Image.asset("assets/coffee_cup.png"),
-                ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Cappuccino",
-                        style: TextStyle(
-                          fontSize: 25,
+    return StreamBuilder<List<MenuModel>>(
+        stream: Brewdatabase().menuStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<MenuModel>? menuData = snapshot.data;
+            return ListView.builder(
+              itemCount: menuData!.length,
+              itemBuilder: (context, index) => Column(
+                children: [
+                  //Coffee part
+                  Container(
+                    height: 150,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    // decoration: BoxDecoration(
+                    //   color: customGreyColor,
+                    //   borderRadius: BorderRadius.circular(15),
+                    //   boxShadow: customShadow,
+                    // ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            padding: const EdgeInsets.all(15),
+                            height: 100,
+                            margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                            decoration: BoxDecoration(
+                              color: coffeeColor,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Image.asset("assets/coffee_cup.png"),
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        "\$1.5",
-                        style: TextStyle(
-                          fontSize: 17,
+                        Expanded(
+                          flex: 3,
+                          child: Container(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Cappuccino",
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  "\$1.5",
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
                         ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ),
-            ],
-          ),
-        ),
 
-        const Divider(
-          indent: 40,
-          endIndent: 40,
-        ),
-        //Coffee part
-        Container(
-          height: 150,
-          margin: const EdgeInsets.symmetric(horizontal: 10),
-          // decoration: BoxDecoration(
-          //   color: customGreyColor,
-          //   borderRadius: BorderRadius.circular(15),
-          //   boxShadow: customShadow,
-          // ),
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  padding: const EdgeInsets.all(15),
-                  height: 100,
-                  margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                  decoration: BoxDecoration(
-                    color: coffeeColor,
-                    borderRadius: BorderRadius.circular(20),
+                  const Divider(
+                    indent: 40,
+                    endIndent: 40,
                   ),
-                  child: Image.asset("assets/coffee_cup.png"),
-                ),
+                  //Dummy data
+                ],
               ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Espresso",
-                        style: TextStyle(
-                          fontSize: 25,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        "\$1.0",
-                        style: TextStyle(
-                          fontSize: 17,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
+            );
+          } else {
+            return const LoadingPage(text: "");
+          }
+        });
   }
 }
--- flutter/firebase_test/brew_crew/lib/Services/brewdb.dart
+++ flutter/firebase_test/brew_crew/lib/Services/brewdb.dart
@@ -1,8 +1,24 @@
+import 'package:brew_crew/Models/menu_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class Brewdatabase {
   final CollectionReference brewCollection =
       FirebaseFirestore.instance.collection("menu");
+
+  List<MenuModel> customMenuModel(QuerySnapshot snapshot) {
+    return snapshot.docs.map((eachDocument) {
+      // print(eachDocument);
+      return MenuModel(
+          name: eachDocument.toString(),
+          price: eachDocument['price'],
+          about: eachDocument['about']);
+    }).toList();
+  }
+
+  Stream<List<MenuModel>> get menuStream {
+    return brewCollection.snapshots().map(customMenuModel);
+  }
 
   //For barista
   Future updateCoffee(String name, double price, String about) async {
