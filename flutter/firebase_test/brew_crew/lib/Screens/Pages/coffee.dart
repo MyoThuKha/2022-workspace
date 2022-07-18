f1896431dc28c36ee3b0bcbb4ed34eac095f6946
MyoThuKha <myothukha@outlook.sg>
2022-07-18 20:22:30 +0630

    fixed listview overflow

--- flutter/firebase_test/brew_crew/lib/Screens/Pages/coffee.dart
+++ flutter/firebase_test/brew_crew/lib/Screens/Pages/coffee.dart
@@ -23,70 +23,72 @@ class _CoffeeListState extends State<CoffeeList> {
             return Text("No Data here");
           }
           List<MenuModel>? menuData = snapshot.data;
-          return ListView.builder(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemCount: menuData?.length,
-            itemBuilder: (context, index) => Column(
-              children: [
-                //Coffee part
-                Container(
-                  height: 150,
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  // decoration: BoxDecoration(
-                  //   color: customGreyColor,
-                  //   borderRadius: BorderRadius.circular(15),
-                  //   boxShadow: customShadow,
-                  // ),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          padding: const EdgeInsets.all(15),
-                          height: 100,
-                          margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                          decoration: BoxDecoration(
-                            color: coffeeColor,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Image.asset("assets/coffee_cup.png"),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 3,
-                        child: Container(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                menuData![index].name,
-                                style: const TextStyle(
-                                  fontSize: 25,
-                                ),
-                              ),
-                              const SizedBox(height: 20),
-                              Text(
-                                menuData[index].price.toString(),
-                                style: const TextStyle(
-                                  fontSize: 17,
-                                ),
-                              )
-                            ],
+          return Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: menuData?.length,
+              itemBuilder: (context, index) => Column(
+                children: [
+                  //Coffee part
+                  Container(
+                    height: 150,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    // decoration: BoxDecoration(
+                    //   color: customGreyColor,
+                    //   borderRadius: BorderRadius.circular(15),
+                    //   boxShadow: customShadow,
+                    // ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            padding: const EdgeInsets.all(15),
+                            height: 100,
+                            margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                            decoration: BoxDecoration(
+                              color: coffeeColor,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Image.asset("assets/coffee_cup.png"),
                           ),
                         ),
-                      ),
-                    ],
+                        Expanded(
+                          flex: 3,
+                          child: Container(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  menuData![index].name,
+                                  style: const TextStyle(
+                                    fontSize: 25,
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                Text(
+                                  menuData[index].price.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 17,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
 
-                const Divider(
-                  indent: 40,
-                  endIndent: 40,
-                ),
-                //Dummy data
-              ],
+                  const Divider(
+                    indent: 40,
+                    endIndent: 40,
+                  ),
+                  //Dummy data
+                ],
+              ),
             ),
           );
           // return Center(
