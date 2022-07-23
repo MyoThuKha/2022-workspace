be5392867e98d5442171c293f963afb163e74056
MyoThuKha <myothukha@outlook.sg>
2022-07-23 18:38:42 +0630

    can update order

--- flutter/firebase_test/brew_crew/lib/Screens/Pages/detail.dart
+++ flutter/firebase_test/brew_crew/lib/Screens/Pages/detail.dart
@@ -1,7 +1,12 @@
+import 'package:brew_crew/Models/brew.dart';
+import 'package:brew_crew/Models/user_model.dart';
+import 'package:brew_crew/Services/database.dart';
 import 'package:brew_crew/Templates/colors.dart';
 import 'package:brew_crew/Templates/constants.dart';
+import 'package:brew_crew/Templates/loading.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({Key? key}) : super(key: key);
@@ -12,6 +17,194 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   int _current = 1;
+
+  bool isFav = false;
+  @override
+  Widget build(BuildContext context) {
+    Map menuData = ModalRoute.of(context)?.settings.arguments as Map;
+    UserModel? user = Provider.of(context);
+    return StreamBuilder<BrewModel>(
+        stream: DatabaseService(uid: user!.uid).userDocStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            BrewModel? userData = snapshot.data;
+            return Scaffold(
+              backgroundColor: customGreyColor,
+              body: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 0,
+                    child: Container(
+                      width: deviceWidth,
+                      height: deviceHeight * 3 / 7,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/coffee2.jpeg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 50,
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(CupertinoIcons.back,
+                            color: Colors.white)),
+                  ),
+                  Positioned(
+                    top: 50,
+                    right: 0,
+                    child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isFav = !isFav;
+                          });
+                        },
+                        icon: Icon(
+                            isFav ? Icons.favorite : Icons.favorite_border,
+                            color: isFav ? Colors.red : Colors.white)),
+                  ),
+                  Positioned(
+                    top: deviceHeight * 3 / 7 - 30,
+                    child: Container(
+                      width: deviceWidth,
+                      height: deviceHeight * 4 / 7 + 30,
+                      //for content spacing
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 20),
+                      decoration: BoxDecoration(
+                        color: customGreyColor,
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(30)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //main header
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                menuData['name'],
+                                style: const TextStyle(
+                                  fontSize: 38,
+                                  letterSpacing: 1,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                "\$ ${menuData['price'].toString()}",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  color: coffeeColor,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                          // const SizedBox(height: 10),
+
+                          //how many size
+
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Size Options",
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  letterSpacing: 1,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  appButton(1),
+                                  const SizedBox(width: 15),
+                                  appButton(2),
+                                  const SizedBox(width: 15),
+                                  appButton(3),
+                                ],
+                              ),
+                            ],
+                          ),
+
+                          //Description section
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Description",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  letterSpacing: 1,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                menuData['about'],
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  letterSpacing: 1,
+                                  color: Colors.grey[700],
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          //order buttton
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: Material(
+                              color: coffeeColor,
+                              child: InkWell(
+                                splashColor: Colors.grey,
+                                onTap: () async {
+                                  await DatabaseService(uid: user.uid)
+                                      .updateUserData(userData!.name,
+                                          userData.barista, menuData['name']);
+                                },
+                                child: Container(
+                                  alignment: Alignment.bottomCenter,
+                                  width: double.maxFinite,
+                                  height: 65,
+                                  // decoration: BoxDecoration(
+                                  // color: Colors.orange,
+                                  // borderRadius: BorderRadius.circular(30),
+                                  // ),
+                                  child: const Center(
+                                    child: Text(
+                                      "Order",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const LoadingPage(text: "Please Wait");
+          }
+        });
+  }
+
+  //custom buttom for size
   Widget appButton(int id) {
     return GestureDetector(
       onTap: () {
@@ -31,167 +224,9 @@ class _DetailPageState extends State<DetailPage> {
             '$id',
             style: TextStyle(
                 fontSize: 25,
-                color: id == _current ? Colors.white : Colors.black),
+                color: id == _current ? Colors.white : Colors.grey[850]),
           ),
         ),
-      ),
-    );
-  }
-
-  bool isFav = false;
-  @override
-  Widget build(BuildContext context) {
-    Map data = ModalRoute.of(context)?.settings.arguments as Map;
-    return Scaffold(
-      backgroundColor: customGreyColor,
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            child: Container(
-              width: deviceWidth,
-              height: deviceHeight * 3 / 7,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/coffee2.jpeg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 50,
-            child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(CupertinoIcons.back, color: Colors.white)),
-          ),
-          Positioned(
-            top: 50,
-            right: 0,
-            child: IconButton(
-                onPressed: () {
-                  setState(() {
-                    isFav = !isFav;
-                  });
-                },
-                icon: Icon(isFav ? Icons.favorite : Icons.favorite_border,
-                    color: isFav ? Colors.red : Colors.white)),
-          ),
-          Positioned(
-            top: deviceHeight * 3 / 7 - 30,
-            child: Container(
-              width: deviceWidth,
-              height: deviceHeight * 4 / 7 + 30,
-              //for content spacing
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              decoration: BoxDecoration(
-                color: customGreyColor,
-                borderRadius:
-                    const BorderRadius.vertical(top: Radius.circular(30)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //main header
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        data['name'],
-                        style: const TextStyle(
-                          fontSize: 38,
-                          letterSpacing: 1,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Text(
-                        "\$ ${data['price'].toString()}",
-                        style: TextStyle(
-                          fontSize: 30,
-                          color: coffeeColor,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // const SizedBox(height: 10),
-
-                  //how many size
-
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Size Options",
-                        style: TextStyle(
-                          fontSize: 25,
-                          letterSpacing: 1,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          appButton(1),
-                          const SizedBox(width: 15),
-                          appButton(2),
-                          const SizedBox(width: 15),
-                          appButton(3),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  //Description section
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Description",
-                        style: TextStyle(
-                          fontSize: 30,
-                          letterSpacing: 1,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        data['about'],
-                        style: TextStyle(
-                          fontSize: 17,
-                          letterSpacing: 1,
-                          color: Colors.grey[700],
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  //order buttton
-                  Container(
-                    alignment: Alignment.bottomCenter,
-                    width: double.maxFinite,
-                    height: 65,
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        "Order",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
