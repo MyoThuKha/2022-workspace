ff4a749ff19f401de6cdc1c03dddb3e3ac156b77
MyoThuKha <myothukha@outlook.sg>
2022-08-01 16:42:46 +0630

    need fix total

--- flutter/firebase_test/brew_crew/lib/Screens/Pages/orders.dart
+++ flutter/firebase_test/brew_crew/lib/Screens/Pages/orders.dart
@@ -30,132 +30,133 @@ class _OrderPageState extends State<OrderPage> {
       child: ListView.builder(
         itemCount: userData.brew.length + 1,
         itemBuilder: (context, index) {
-          return index <= userData.brew.length - 1
-              ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      height: 150,
-                      margin: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            flex: 2,
-                            child: Container(
-                              padding: const EdgeInsets.all(15),
-                              height: 100,
-                              margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                              decoration: BoxDecoration(
-                                color: coffeeColor,
-                                borderRadius: BorderRadius.circular(20),
-                                // image: const DecorationImage(
-                                //   image:
-                                //       AssetImage("assets/coffee_cup.png"),
-                                //   fit: BoxFit.contain,
-                                // ),
-                              ),
-                              child: Image.asset("assets/coffee_cup.png"),
-                            ),
+          if (index <= userData.brew.length - 1) {
+            total = total + userData.brew[index]["price"];
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: 150,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          height: 100,
+                          margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                          decoration: BoxDecoration(
+                            color: coffeeColor,
+                            borderRadius: BorderRadius.circular(20),
+                            // image: const DecorationImage(
+                            //   image:
+                            //       AssetImage("assets/coffee_cup.png"),
+                            //   fit: BoxFit.contain,
+                            // ),
                           ),
-                          Expanded(
-                            flex: 3,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 10),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Flexible(
-                                        child: Text(
-                                          userData.brew[index]['name'],
-                                          style: const TextStyle(
-                                            fontSize: 25,
-                                          ),
-                                        ),
-                                      ),
-                                      IconButton(
-                                          onPressed: () async {
-                                            setState(() {
-                                              orders!
-                                                  .remove(userData.brew[index]);
-                                            });
-                                            await DatabaseService(
-                                                    uid: user!.uid)
-                                                .updateUserData(
-                                              userData.name,
-                                              userData.barista,
-                                              orders!,
-                                              userData.favorite,
-                                            );
-                                          },
-                                          icon:
-                                              const Icon(CupertinoIcons.trash)),
-                                    ],
-                                  ),
-                                ),
-                                const SizedBox(height: 15),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    // Text(
-                                    //   "\$ ${userData.cost[0]}",
-                                    //   style: const TextStyle(
-                                    //     fontSize: 17,
-                                    //   ),
-                                    // ),
-                                    Text(
-                                      sizeList[userData.brew[index]["size"]],
+                          child: Image.asset("assets/coffee_cup.png"),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(right: 10),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      userData.brew[index]['name'],
                                       style: const TextStyle(
-                                        fontSize: 17,
+                                        fontSize: 25,
                                       ),
                                     ),
-                                  ],
+                                  ),
+                                  IconButton(
+                                      onPressed: () async {
+                                        setState(() {
+                                          total = total -
+                                              userData.brew[index]['price'];
+                                          orders!.remove(userData.brew[index]);
+                                        });
+                                        await DatabaseService(uid: user!.uid)
+                                            .updateUserData(
+                                          userData.name,
+                                          userData.barista,
+                                          orders!,
+                                          userData.favorite,
+                                        );
+                                      },
+                                      icon: const Icon(CupertinoIcons.trash)),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                // Text(
+                                //   "\$ ${userData.cost[0]}",
+                                //   style: const TextStyle(
+                                //     fontSize: 17,
+                                //   ),
+                                // ),
+                                Text(
+                                  sizeList[userData.brew[index]["size"]],
+                                  style: const TextStyle(
+                                    fontSize: 17,
+                                  ),
                                 ),
                               ],
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Divider(),
-                  ],
-                )
-              : Container(
-                  height: 100,
-                  margin: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        "Total : ",
-                        style: TextStyle(
-                          fontSize: 40,
-                          color: coffeeColor,
-                          letterSpacing: 1,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Flexible(
-                        child: Text(
-                          // (userData.brew.isEmpty)
-                          // ? "${userData.cost[0] + userData.cost[1]}"
-                          // : "${userData.cost[0] + userData.cost[1] + (2 * (userData.size - 1))}",
-                          "$total",
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w400,
-                            color: coffeeColor,
-                          ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                );
+                ),
+                const Divider(),
+              ],
+            );
+          } else {
+            return Container(
+              height: 100,
+              margin: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "Total : ",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: coffeeColor,
+                      letterSpacing: 1,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Flexible(
+                    child: Text(
+                      // (userData.brew.isEmpty)
+                      // ? "${userData.cost[0] + userData.cost[1]}"
+                      // : "${userData.cost[0] + userData.cost[1] + (2 * (userData.size - 1))}",
+                      "$total \$",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w400,
+                        color: coffeeColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
         },
       ),
     );
