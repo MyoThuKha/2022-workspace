7014b1d4aae810663ec18e9a9dd874518cec36c2
MyoThuKha <myothukha@outlook.sg>
2022-08-05 11:39:56 +0630

    use bootstrap

--- react/react-app/src/components/Counter.jsx
+++ react/react-app/src/components/Counter.jsx
@@ -3,14 +3,12 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imgUrl: "https://picsum.photos/200",
   };
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.imgUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge bg-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
