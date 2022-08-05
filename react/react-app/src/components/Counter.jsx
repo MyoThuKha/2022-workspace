b1020e7bceadffdb71837766254270b07d411af2
MyoThuKha <myothukha@outlook.sg>
2022-08-05 10:22:30 +0630

    ...

--- react/react-app/src/components/Counter.jsx
+++ react/react-app/src/components/Counter.jsx
@@ -3,14 +3,21 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imgUrl: "https://picsum.photos/200",
   };
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
+        <img src={this.state.imgUrl} alt="" />
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
