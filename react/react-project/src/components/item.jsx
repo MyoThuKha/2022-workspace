eea4f4b405d6bc9c37d4ea40098be0b761e565e6
MyoThuKha <myothukha@outlook.sg>
2022-08-08 18:52:09 +0630

    ...

--- react/react-project/src/components/item.jsx
+++ react/react-project/src/components/item.jsx
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
-
+import image from "../assets/menu/A/A.jpg";
 class Item extends Component {
   state = {};
   render() {
-    return <div className="badge bg-primary m-3">Milk</div>;
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="row justify-content-around">
+            <div className="col-3">
+              <div className="container bg-white rounded shadow m-2">
+                <div className="pt-2 pb-5 justify-content-center">
+                  <img className="rounded mx-auto d-block" src={image} alt="" />
+                </div>
+                <div className="lead justify-content-center">Ramen</div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="container bg-white rounded shadow m-2">
+                <div className="p-2 pb-5 justify-content-center">
+                  <img className="rounded mx-auto d-block" src={image} alt="" />
+                </div>
+                <div className="lead justify-content-center">Ramen</div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="container bg-white rounded shadow m-2">
+                <div className="p-2 pb-5 justify-content-center">
+                  <img className="rounded mx-auto d-block" src={image} alt="" />
+                </div>
+                <div className="lead justify-content-center">Ramen</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
--- react/react-project/src/components/navbar.jsx
+++ react/react-project/src/components/navbar.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
-
+import image from "../assets/main_image.jpg";
 class NavBar extends Component {
   state = {};
+  style = {
+    height: "800px",
+    backgroundImage: `url(${image})`,
+    backgroundSize: "100% 800px",
+  };
   render() {
     return (
-      <nav className="navbar navbar-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href=".">
-            <div className="lead">Home Page</div>
-          </a>
+      <React.Fragment>
+        <div style={this.style} className="">
+          <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+              <a className="navbar-brand" href=".">
+                <div className="lead">Home Page</div>
+              </a>
+            </div>
+          </nav>
         </div>
-      </nav>
+      </React.Fragment>
     );
   }
 }
