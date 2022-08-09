0860b05aab34963da6ba1ba469eaaf718be34286
MyoThuKha <myothukha@outlook.sg>
2022-08-09 21:00:57 +0630

    added navbar toggler

--- react/fm-project/src/components/about.jsx
+++ react/fm-project/src/components/about.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const About = () => {
   return (
     <React.Fragment>
-      <div className="display-2 text-white">Hello, I am Myo Thu Kha.</div>
+      <div id="about">
+        <div className="display-2 text-white">Hello, I am Myo Thu Kha.</div>
+      </div>
     </React.Fragment>
   );
 };
--- react/fm-project/src/components/navbar.jsx
+++ react/fm-project/src/components/navbar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const NavBar = () => {
   return (
     <React.Fragment>
-      <nav className="navbar navbar-expand-md navbar-black bg-black border-bottom p-3">
-        <div className="container-xxl">
+      <nav className="navbar navbar-expand-md navbar-dark bg-black border-bottom p-3">
+        <div className="container-xxl justify-content-end">
           {/* <a href="." className="navbar-brand ">
             <span className="text-white ">Home</span>
           </a> */}
@@ -13,22 +13,22 @@ const NavBar = () => {
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#main-nav"
+            data-bs-target=".main-nav"
             aria-controls="main-nav"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon text-white"></span>
           </button>
-          <div className="collapse navbar-collapse" id="main-nav">
+          <div className="collapse navbar-collapse main-nav">
             <ul className="navbar-nav">
               <li className="navbar-item">
-                <a href="." className="nav-link text-white">
+                <a href="#home" className="nav-link text-white">
                   Home
                 </a>
               </li>
               <li className="navbar-item">
-                <a href="." className="nav-link text-white">
+                <a href="#about" className="nav-link text-white">
                   About
                 </a>
               </li>
@@ -48,6 +48,19 @@ const NavBar = () => {
                 </a>
               </li>
             </ul>
+          </div>
+
+          {/* Contact */}
+          <div
+            className="collapse navbar-collapse justify-content-end main-nav"
+            // id="main-nav"
+          >
+            <a
+              href="mailto:myothukha@outlook.sg"
+              className="nav-link text-white"
+            >
+              Contact
+            </a>
           </div>
         </div>
       </nav>
