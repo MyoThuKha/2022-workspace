9888052d30f0d917602acc59a6164a306891aefa
MyoThuKha <myothukha@outlook.sg>
2022-08-10 21:05:43 +0630

    move contact

--- react/fm-project/src/components/navbar.jsx
+++ react/fm-project/src/components/navbar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <React.Fragment>
       <nav className="navbar sticky-top navbar-expand-md navbar-dark bg-black border-bottom p-3">
-        <div className="container-xxl justify-content-end">
+        <div className="container-fluid justify-content-end">
           {/* <a href="." className="navbar-brand ">
             <span className="text-white ">Home</span>
           </a> */}
@@ -13,54 +13,48 @@ const NavBar = () => {
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target=".main-nav"
-            aria-controls="main-nav"
+            data-bs-target="#mainNav"
+            aria-controls="mainNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon text-white"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse main-nav">
-            <ul className="navbar-nav">
-              <li className="navbar-item">
-                <a href="#home" className="nav-link text-white">
-                  Home
-                </a>
-              </li>
-              <li className="navbar-item">
-                <a href="#about" className="nav-link text-white">
-                  About
-                </a>
-              </li>
-              <li className="navbar-item">
-                <a
-                  href="https://github.com/MyoThuKha"
-                  className="nav-link text-white"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Projects
-                </a>
-              </li>
-              <li className="navbar-item">
-                <a href="." className="nav-link text-white">
-                  Blog
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          {/* Contact */}
           <div
-            className="collapse navbar-collapse justify-content-end main-nav"
-            // id="main-nav"
+            className="collapse navbar-collapse justify-content-between"
+            id="mainNav"
           >
-            <a
-              href="mailto:myothukha@outlook.sg"
-              className="nav-link text-white"
-            >
-              Contact
-            </a>
+            {/* nav items */}
+
+            <div className="navbar-nav">
+              <a href="#home" className="nav-link text-white">
+                Home
+              </a>
+              <a href="#about" className="nav-link text-white">
+                About
+              </a>
+              <a
+                href="https://github.com/MyoThuKha"
+                className="nav-link text-white"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Projects
+              </a>
+              <a href="." className="nav-link text-white">
+                Blog
+              </a>
+            </div>
+
+            {/* Contact */}
+            <div className="navbar-nav justify-content-end">
+              <a
+                href="mailto:myothukha.dev@gmail.com"
+                className="nav-link text-white"
+              >
+                Contact
+              </a>
+            </div>
           </div>
         </div>
       </nav>
