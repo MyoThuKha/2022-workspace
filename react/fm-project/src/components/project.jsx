b2a99c60b4e48d23eaf5babf76cc11d51bb064b5
MyoThuKha <myothukha@outlook.sg>
2022-08-10 22:17:29 +0630

    under development

---
+++ react/fm-project/src/components/project.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <React.Fragment>
+      <div className="container-fluid" id="projects">
+        <div
+          style={{
+            padingTop: 140,
+            marginBottom: 20,
+          }}
+          className="text-white h3"
+        >
+          Simple Note App
+        </div>
+        <div className="text-white lead">
+          Note taking app created with flutter.
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Projects;
