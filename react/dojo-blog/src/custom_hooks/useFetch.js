50ef1ce6f07921b9fafe437d984ffc1487239c2d
MyoThuKha <myothukha@outlook.sg>
2022-08-16 10:38:29 +0630

    useEffect cleanup

--- react/dojo-blog/src/custom_hooks/useFetch.js
+++ react/dojo-blog/src/custom_hooks/useFetch.js
@@ -6,22 +6,32 @@ const useFetch = (url) => {
   const [errText, setErrText] = useState(null);
   useEffect(() => {
     //dont change state here, will run infinite loop
-    fetch(url)
-      .then((res) => {
-        //if data not exist
-        if (res.status !== 200) {
-          throw Error("wrong fetch");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setPending(false);
-      })
-      .catch((err) => {
-        setErrText(err.message);
-        setPending(false);
-      });
+    const abortCtrl = new AbortController();
+    setTimeout(() => {
+      fetch(url, { signal: abortCtrl.signal })
+        .then((res) => {
+          //if data not exist
+          if (res.status !== 200) {
+            throw Error("wrong fetch");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setPending(false);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("Fetching Aborted");
+          } else {
+            setErrText(err.message);
+            setPending(false);
+          }
+        });
+    }, 1000);
+    return () => {
+      abortCtrl.abort();
+    };
     //run only when url change
   }, [url]);
   return { data, pending, errText };
