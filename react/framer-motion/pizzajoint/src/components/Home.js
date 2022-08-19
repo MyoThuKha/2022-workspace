b2f4bf0cbb3a25b099786f3f65594aeef3cc5776
MyoThuKha <myothukha@outlook.sg>
2022-08-19 20:46:23 +0630

    initial state

--- react/framer-motion/pizzajoint/src/components/Home.js
+++ react/framer-motion/pizzajoint/src/components/Home.js
@@ -3,18 +3,11 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 const Home = () => {
   return (
-    <motion.div
-      animate={{ rotateZ: 180, marginTop: 300 }}
-      className="home container"
-    >
+    <motion.div className="home container">
       {/* x positive left / negative right y positive down negative up */}
-      <motion.h2 animate={{ fontSize: 50, color: "#00ffff", x: 100, y: -100 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+      <h2 animate={{}}>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button animate={{ opacity: 8, scale: 1.5 }}>
-          Create Your Pizza
-        </motion.button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
     </motion.div>
   );
