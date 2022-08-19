12d32b00c719904dbcfdfe604005d49d070fd494
MyoThuKha <myothukha@outlook.sg>
2022-08-19 20:33:14 +0630

    motion

--- react/framer-motion/pizzajoint/src/components/Home.js
+++ react/framer-motion/pizzajoint/src/components/Home.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      animate={{ rotateZ: 180, marginTop: 300 }}
+      className="home container"
+    >
+      {/* x positive left / negative right y positive down negative up */}
+      <motion.h2 animate={{ fontSize: 50, color: "#00ffff", x: 100, y: -100 }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button animate={{ opacity: 8, scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
