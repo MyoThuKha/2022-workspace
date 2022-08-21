2a1944f56d65c0e3619533a2ae99d82306cc8ac5
MyoThuKha <myothukha@outlook.sg>
2022-08-21 17:13:52 +0630

    start

---
+++ react/project0/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/project0/src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-light bg-light">
+        <div className="navbar-brand">Hello</div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default Header;
