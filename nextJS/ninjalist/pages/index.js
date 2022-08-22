b436a975cde7681c19982d63bd9b2a697e9b3409
MyoThuKha <myothukha@outlook.sg>
2022-08-22 20:15:51 +0630

    global css

--- nextJS/ninjalist/pages/index.js
+++ nextJS/ninjalist/pages/index.js
@@ -1,10 +1,19 @@
-import NavBar from "../components/navbar";
-import FooterBar from "../components/footer";
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <div>Lorem ipsum dolor sit amet consectetur.</div>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
+        pariatur minus ea eligendi asperiores suscipit aut molestiae porro
+        consequuntur corporis, dolore amet iste repellendus ipsam, ducimus totam
+        labore nobis omnis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
+        pariatur minus ea eligendi asperiores suscipit aut molestiae porro
+        consequuntur corporis, dolore amet iste repellendus ipsam, ducimus totam
+        labore nobis omnis?
+      </p>
     </div>
   );
 }
