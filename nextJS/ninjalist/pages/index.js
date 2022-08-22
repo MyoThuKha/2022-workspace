13e5ee1f2c7b5df2245776ce35b5597eebf3a0a3
MyoThuKha <myothukha@outlook.sg>
2022-08-22 20:34:20 +0630

    module css

--- nextJS/ninjalist/pages/index.js
+++ nextJS/ninjalist/pages/index.js
@@ -1,19 +1,25 @@
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
+    <div className={styles.title}>
       <h1>Home</h1>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
         pariatur minus ea eligendi asperiores suscipit aut molestiae porro
         consequuntur corporis, dolore amet iste repellendus ipsam, ducimus totam
         labore nobis omnis?
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
         pariatur minus ea eligendi asperiores suscipit aut molestiae porro
         consequuntur corporis, dolore amet iste repellendus ipsam, ducimus totam
         labore nobis omnis?
       </p>
+      <Link href="/ninjas">
+        <a className={styles.btn}>Go to Ninja Listing</a>
+      </Link>
     </div>
   );
 }
