e3c3720ef9c3b23dac9283e08f08228feb9e98f3
MyoThuKha <myothukha@outlook.sg>
2022-08-22 20:50:47 +0630

    redirect

--- nextJS/ninjalist/pages/404.js
+++ nextJS/ninjalist/pages/404.js
@@ -1,6 +1,14 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const NotFound = () => {
+  const route = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      route.push("/");
+    }, 4000);
+  }, [route]);
   return (
     <div className="notFound">
       <h1>Page not Found</h1>
