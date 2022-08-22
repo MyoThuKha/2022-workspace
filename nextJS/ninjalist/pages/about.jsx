bc8a1bdfbb6d7c47f848e8a0c6aa24bcbf043e56
MyoThuKha <myothukha@outlook.sg>
2022-08-22 09:37:15 +0630

    nextjs

---
+++ nextJS/ninjalist/pages/about.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo architecto
+        error ad ipsam facilis dolores impedit natus, laudantium suscipit
+        repudiandae fuga similique mollitia voluptatum ducimus quod debitis
+        dolore consequatur quidem labore iusto aliquam delectus vel atque iste.
+        Laborum impedit aliquid accusantium voluptatum repellendus et
+        consequuntur molestias, delectus sint soluta praesentium?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo architecto
+        error ad ipsam facilis dolores impedit natus, laudantium suscipit
+        repudiandae fuga similique mollitia voluptatum ducimus quod debitis
+        dolore consequatur quidem labore iusto aliquam delectus vel atque iste.
+        Laborum impedit aliquid accusantium voluptatum repellendus et
+        consequuntur molestias, delectus sint soluta praesentium?
+      </p>
+    </React.Fragment>
+  );
+};
+
+export default About;
