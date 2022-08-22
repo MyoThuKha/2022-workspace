8796ccb4ba11c19076019192af89afa6c4420716
MyoThuKha <myothukha@outlook.sg>
2022-08-22 21:44:29 +0630

    getStaticProps

--- nextJS/ninjalist/pages/ninjas/index.jsx
+++ nextJS/ninjalist/pages/ninjas/index.jsx
@@ -1,5 +1,27 @@
-const Test = () => {
-  return <h1>This page is testing.</h1>;
+import React from "react";
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { data },
+  };
+};
+
+const Test = ({ data: ninjas }) => {
+  return (
+    <React.Fragment>
+      <h1>Ninjas</h1>
+      {ninjas.map((each) => (
+        <div key={each.id}>
+          <a className={styles.single}>
+            <h3>{each.name}</h3>
+          </a>
+        </div>
+      ))}
+    </React.Fragment>
+  );
 };
 
 export default Test;
