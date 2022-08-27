6dfa40c53c77d05d4c223697d4064cf32bcba2bb
MyoThuKha <myothukha@outlook.sg>
2022-08-27 09:38:22 +0630

    add icon

--- reactNative/myproject/App.js
+++ reactNative/myproject/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text style={styles.header}>Hello world</Text>
       <Text>Open up App.js to start working on your app!!!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,5 +17,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    fontSize: "30px",
+    fontWeight: "bold",
   },
 });
