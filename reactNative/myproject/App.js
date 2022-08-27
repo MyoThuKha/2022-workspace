b0dec2354da29bdce784653d955638c246f66f82
MyoThuKha <myothukha@outlook.sg>
2022-08-27 17:39:47 +0630

    touchableOpacity

--- reactNative/myproject/App.js
+++ reactNative/myproject/App.js
@@ -7,28 +7,39 @@ import {
   Button,
   TextInput,
   ScrollView,
+  FlatList,
+  TouchableOpacity,
 } from "react-native";
 
 export default function App() {
   const [items, setItems] = useState([
-    { name: "John", key: 1 },
-    { name: "Sam", key: 2 },
-    { name: "Anon", key: 3 },
-    { name: "Olivia", key: 4 },
-    { name: "James", key: 5 },
-    { name: "May", key: 6 },
+    { name: "John", id: 1 },
+    { name: "Sam", id: 2 },
+    { name: "Anon", id: 3 },
+    { name: "Olivia", id: 4 },
+    { name: "James", id: 5 },
+    { name: "May", id: 6 },
   ]);
+
+  const onTouchHandler = (id) => {
+    setItems((origin) => {
+      return origin.filter((item) => item.id != id);
+    });
+  };
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {items.map((item) => {
-          return (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        horizontal={false}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => onTouchHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+        //destructuring props
+      />
     </View>
   );
 }
@@ -37,13 +48,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
+    alignItems: "center",
     // justifyContent: "center",
   },
   item: {
     color: "white",
     backgroundColor: "#c68642",
     height: 100,
+    width: 150,
     borderRadius: 8,
     margin: 30,
     padding: 30,
