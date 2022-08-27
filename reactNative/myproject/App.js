cce44fec2779ecbfd40cd3cc451be79c700a0bd8
MyoThuKha <myothukha@outlook.sg>
2022-08-27 14:28:51 +0630

    textinput

--- reactNative/myproject/App.js
+++ reactNative/myproject/App.js
@@ -1,15 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Welcome");
+  const [name, setName] = useState("John");
+  const [age, setAge] = useState("18");
   const clickHandler = () => {
     setName("My name is Myo Thu Kha.");
   };
   return (
     <View style={styles.container}>
-      <Text>{name}</Text>
+      <Text style={styles.header}>{name}</Text>
+      <View>
+        <TextInput
+          placeholder="Name"
+          onChangeText={(val) => setName(val)}
+          style={styles.input}
+        />
+      </View>
+      <View>
+        <Text style={styles.header}>{age}</Text>
+        <View>
+          <TextInput
+            placeholder="Age"
+            keyboardType="email"
+            onChangeText={(val) => setAge(val)}
+            style={styles.input}
+          />
+        </View>
+      </View>
+
+      {/* // */}
       <View style={styles.button}>
         <Button title="Change" onPress={clickHandler} />
       </View>
@@ -26,9 +47,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   header: {
-    fontWeight: "bold",
+    fontSize: 30,
+    margin: 30,
   },
   button: {
     marginTop: 10,
+  },
+  input: {
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+    width: 350,
+    height: 50,
   },
 });
