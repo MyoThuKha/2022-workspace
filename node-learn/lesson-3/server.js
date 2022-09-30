e0a1c9286efd521b7397ceeed7729ae278845294
MyoThuKha <myothukha@outlook.sg>
2022-09-30 15:30:53 +0630

    html

--- node-learn/lesson-3/server.js
+++ node-learn/lesson-3/server.js
@@ -1,7 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  console.log(req.url, req.method);
+
+  //
+  res.setHeader("Content-Type", "text/html");
+  //get html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      res.end();
+      console.log(err);
+    }
+    //if write is only one,can use end
+    else res.end(data);
+  });
 });
 
 server.listen(3000, "localhost", () => {
