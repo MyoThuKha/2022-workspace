a4fccf3c112abe6b9c80b9b22f4da20f282e9b38
MyoThuKha <myothukha@outlook.sg>
2022-10-03 18:34:49 +0630

    custom middleware

--- node-learn/lesson-3/app.js
+++ node-learn/lesson-3/app.js
@@ -6,6 +6,12 @@ app.set("view engine", "ejs");
 app.set("views", "pages");
 
 app.listen(3000);
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
