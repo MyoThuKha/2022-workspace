209c4fb2bb92b2a861d926fffd8c88d5dffd66ac
MyoThuKha <myothukha@outlook.sg>
2022-10-03 17:58:55 +0630

    using ejs

--- node-learn/lesson-3/app.js
+++ node-learn/lesson-3/app.js
@@ -2,20 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "pages");
+
 app.listen(3000);
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About us" });
 });
-
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/create", (req, res) => {
+  res.render("create", { title: "Create Blogs" });
 });
 
 app.use((req, res) => {
-  res.status(404);
-  res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "Page Not Found" });
 });
